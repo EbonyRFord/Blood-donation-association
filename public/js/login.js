ac7416746e7be31ff41b197fcab74bd691bd320a
Ebony Ford <marketingwithebony@gmail.com>
2023-04-18 10:30:35 -0400

    cleaning up code

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to WHERE????
+      // If successful, redirect the browser to WHERE???? homepage, looks nice
       document.location.replace('/');
     } else {
       alert(response.statusText);
@@ -38,7 +38,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/'); //CHANGE THIS TO A PROFILE PAGE????
+      document.location.replace('/'); //CHANGE THIS TO A PROFILE PAGE???? 2nd layer with options
     } else {
       alert(response.statusText);
     }
