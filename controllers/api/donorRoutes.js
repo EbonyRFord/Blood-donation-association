0e4cbf15c8d27acd8035ceed762f3648d2dd1103
Ebony Ford <marketingwithebony@gmail.com>
2023-04-19 14:28:56 -0400

    remove error displayed in console

--- controllers/api/donorRoutes.js
+++ controllers/api/donorRoutes.js
@@ -2,14 +2,11 @@ const router = require('express').Router();
 const { Donor } = require('../../models');
 
 router.post('/', async (req, res) => {
- 
+
   try {
     const donorData = await Donor.create(req.body);
 
     req.session.save(() => {
-      // req.session.user_id = userData.id;
-      // req.session.logged_in = true;
-
       res.status(200).json(donorData);
     });
   } catch (err) {
